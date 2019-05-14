786233473a5f21b5085eea6ae512274596d75dbd
Johannes Fett <johannes.fett@mailbox.org>
2019-05-14 04:01:12 +0200

    initial commit

---
+++ src/dbops/comp.cpp
@@ -0,0 +1,15 @@
+//
+// Created by johannes on 14.05.19.
+//
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <immintrin.h>
+#include <bitset>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include "comp.h"
+
+
---
+++ src/dbops/comp.h
@@ -0,0 +1,8 @@
+//
+// Created by johannes on 14.05.19.
+//
+
+#ifndef DB_VECTOR_COMP_H
+#define DB_VECTOR_COMP_H
+
+#endif //DB_VECTOR_COMP_H
