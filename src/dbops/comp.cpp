a8ddb3de5698b31cf17ecfde8dc272190f2c2b78
Johannes Fett <johannes.fett@mailbox.org>
2019-05-15 02:47:51 +0200

    cmake for remote initialized. comp dummy works

--- src/dbops/comp.cpp
+++ src/dbops/comp.cpp
@@ -12,9 +12,46 @@
 #include <sstream>
 #include "comp.h"
 
-int main()
-{
+using namespace std;
+int main() {
+
+    //define input and result for compression
+    __m256i a,result;
+
+    //TODO REPLACE WITH VH DATA STREAM
+    //set dummy input data,
+    a= _mm256_setr_epi32(3,5,2,7,11,14,15,16);
+    printf("input\n");
+
+    for(int i =0;i<8;i++){
+        int * in = (int*) &a;
+
+        cout<<in[i]<<" ";
+    }
+    cout<<endl;
+    cout<<"mask:"<<endl;
+   /*
+    * set bitmask for compress operation
+    * 1-element is saved
+    * 0-element is discarded
+    *
+    * example: 0x1A, indices 1,3,4 saved. 2^1 + 2^3 +2^4 = 2+8+16 = 26 = 0x1A (hex)
+    */
+    __mmask8 mask = 0x1A;
+
+    cout<< bitset<8>(0x1A)<<endl;
+
+    result= _mm256_maskz_compress_epi32(mask, a);
+cout<<"output"<<endl;
+    for(int i =0;i<8;i++){
+        int * in = (int*) &result;
+
+        cout<<in[i] <<" ";
+
+    }
+cout<<"WORKS";
 
     return 0;
 }
+
 
