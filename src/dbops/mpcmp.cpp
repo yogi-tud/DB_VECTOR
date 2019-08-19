f9948f4aa201215732ba8e0dd4ee005392b9dc67
Johannes Fett <johannes.fett@mailbox.org>
2019-08-19 07:50:12 +0200

    openMP works.

--- src/dbops/mpcmp.cpp
+++ src/dbops/mpcmp.cpp
@@ -29,9 +29,9 @@ int main() {
     sw.start();
     uint32_t lower = 5000;
     uint32_t upper = 10000;
-    size_t datacount = 256000000;
+    size_t datacount = 1024000000;
     uint8_t * masks = (uint8_t *) aligned_alloc(8, (datacount ) * sizeof(uint8_t));
-    uint64_t * offsets = (uint64_t *) aligned_alloc(8, (datacount / 8) * sizeof(uint64_t));
+
 
     uint64_t *data = (uint64_t *) aligned_alloc(64, datacount * sizeof(uint64_t));
     uint64_t *result = (uint64_t *) aligned_alloc(64, datacount * sizeof(uint64_t));
@@ -44,7 +44,10 @@ int main() {
     int i =0;
     n_per_thread = n/(total_threads);
 
-
+    for(int i =0;i<datacount;i++)
+    {
+        cresult[i]=0;
+    }
 
     int backset =0;
     sw.stop();
@@ -55,7 +58,7 @@ int main() {
 
     for (uint64_t i = 0; i < datacount; ++i) {
         data[i] = rand() % 10000;
-
+       // data[i]=i;
     }
 
     sw.stop();
@@ -97,17 +100,41 @@ int main() {
 
 
 
+    uint64_t * offsets = (uint64_t *) aligned_alloc(8, ((datacount / 8)+1) * sizeof(uint64_t));
     //calc masks and remember offsets
+#pragma omp parallel for shared(result, masks) private(i) schedule(dynamic, n_per_thread)
     for (int i = 0; i < datacount/8 ; i++) {
         masks[i] = createMask(result+8*i);
-        //masks[i]=15;
 
-        offsets[i]= _mm_popcnt_u32( masks[i]);
+        //masks[i]=240;
+
 
     }
+    //masks[31]=0;
     sw.stop();
     double dmask=sw.duration();
-//TODO Umgang mit lücken über threads verteilt. keine lösung möglich? danach memcpys?
+    sw.reset();
+    sw.start();
+    int sum=0;
+
+
+
+    for(int i =0;i<(datacount/8);i++)
+    {
+        offsets[i]=sum;
+
+        sum+=_mm_popcnt_u32( masks[i]);
+
+
+    }
+    offsets[0]=0;
+
+    sw.stop();
+    double doff=sw.duration();
+    //Offsets holds the number of steps the result pointer must be incremented before writing back the compress result
+    //example: mask: 11110000,11110000,11110000,11110000
+    //offsets 0-2: 0,4,8,12
+    //the first element is 0, begin result writing at the start of result buffer
 
 
   sw.reset();
@@ -121,56 +148,41 @@ int main() {
 //jeder thread hat einen definierten bereich beim rückschreiben
 //privaten pointer = threadid * chunksize
 
-   // omp_set_dynamic(0);     // Explicitly disable dynamic teams
+    //omp_set_dynamic(0);     // Explicitly disable dynamic teams
    // omp_set_num_threads(total_threads);
-    /*
+   //TEST CPU LOAD, REMOVE LOOP AFTERWARDS
+   for(int k=0;k<500;k++) {
+
 #pragma omp parallel for shared(cresult, data, masks, offsets) private(i) schedule(dynamic, n_per_thread)
-    for(i=0; i<n/8; i++) {
+       for (i = 0; i < n / 8; i++) {
 
 
+           _mm512_mask_compressstoreu_epi64(cresult + offsets[i], masks[i], _mm512_loadu_si512(data + i * 8));
 
-        _mm512_mask_compressstoreu_epi64(cresult + i * offsets[i], masks[i], _mm512_loadu_si512(data + i * 8) );
-
-    }*/
-
-    for(i=0; i<datacount/8; i++) {
-
-
-
-        _mm512_mask_compressstoreu_epi64( cresult+i*offsets[i],  masks[i], _mm512_loadu_si512(data+i*8) );
-
-    }
+       }
+   }
 
     sw.stop();
     double dcmp = sw.duration();
     cout<<"datacount ; threads "<<datacount<<" "<<total_threads<<endl;
     cout<<"[VH] duration compress "<<dcmp<<endl;
-    cout<<"[VH] duration mask "<<dmask<<endl;
+    cout<<"[VH] duration mask calc "<<dmask<<endl;
+    cout<<"[VH] duration offset calc "<<doff<<endl;
     cout<<"[VH] duration init "<<d_init<<endl;
 /*
-    cout<<"[VH] compress result:"<<endl;
+cout<<"[VH] offsets: "<<endl;
+for(int k=0;k<datacount/8;k++)
+{
+    cout<<offsets[k]<<" ";
+}
+
+    cout<<"\n[VH] cmp | input | index:"<<endl;
     for(int h =0; h < datacount; h++)
     {
-        cout<<(uint64_t)cresult[h]<<" "<<data[h]<<endl;
+        cout<<(uint64_t)cresult[h]<<" "<<data[h]<<" " <<h<<endl;
     }
-    */
-    cout<<"[VH] Lücken test:"<<endl;
-    int fail=0;
-    for(int h =0; h < datacount/8; h++)
-    {
+*/
 
-        if((int)masks[h]==0){
-
-        fail=1;
-    }}
-    if(fail==1)
-    {
-        cout<<"[VH] Compress failed, lücken in result"<<endl;
-    }
-    else
-    {
-        cout<<"[VH] Compress successfull"<<endl;
-    }
 
 
 
